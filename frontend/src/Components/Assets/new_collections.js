bb8c7c5189f8d73e86115ca59d2b70c5895d575f
safran6ix <mohammedsafran6ix@gmail.com>
2025-08-26 14:32:39 +0530

    Adding files

---
+++ frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,69 @@
+import p5_img from './product_5.png'
+import p6_img from './product_6.png'
+import p7_img from './product_7.png'
+import p8_img from './product_8.png'
+import p9_img from './product_9.png'
+import p10_img from './product_10.png'
+import p11_img from './product_11.png'
+import p12_img from './product_12.png'
+
+let data_product = [
+    {
+        id: 5,
+        name: "Men-s Shirt Regular Style Business Casual Long Sleeve Shirts",
+        image: p5_img,
+        new_price: 30.00,
+        old_price: 90.50,
+    },
+    {
+        id: 6,
+        name: "Men's Casual Shirts Print Turn-Down Sleeve Button Stripe Short Check Blouse",
+        image: p6_img,
+        new_price: 50.00,
+        old_price: 80.50,
+    },
+    {
+        id: 7,
+        name: "Women's Long Sleeve Oversize Blouse Casual Long Shirts with Side Pockets S",
+        image: p7_img,
+        new_price: 30.00,
+        old_price: 50.50,
+    },
+    {
+        id: 8,
+        name: "Women's CRAYNER Flowy Halloween Dresses Loose Swing Sun Dress with Pockets, S",
+        image: p8_img,
+        new_price: 10.00,
+        old_price: 18.50,
+    },
+    {
+        id: 9,
+        name: "TAIAOJING Mens Shirts Summer Hawaii Loose Print Turn Down Collar Short Sleeve Blouses Sky Blue L",
+        image: p9_img,
+        new_price: 10.00,
+        old_price: 18.50,
+    },
+    {
+        id: 10,
+        name: "Men's Casual Shirts Print Turn-Down Sleeve Button Stripe Short Check Blouse",
+        image: p10_img,
+        new_price: 50.00,
+        old_price: 80.50,
+    },
+    {
+        id: 11,
+        name: "Women's CRAYNER Flowy Halloween Dresses Loose Swing Sun Dress with Pockets, S",
+        image: p11_img,
+        new_price: 10.00,
+        old_price: 18.50,
+    },
+    {
+        id: 12,
+        name: "Men-s Shirt Regular Style Business Casual Long Sleeve Shirts",
+        image: p12_img,
+        new_price: 30.00,
+        old_price: 90.50,
+    },
+]; 
+
+export default data_product;
